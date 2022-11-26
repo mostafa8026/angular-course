c3f4382cc66899dc67bf6e0aa1e67544e046a935
mostafa <mostafa8026@gmail.com>
2022-11-26 15:02:23 +0330

    fix: update folder structure

---
+++ session-04-class/src/rtk/index.ts
@@ -0,0 +1,33 @@
+import {store} from './app/store'
+import { kindFeature } from './feature/kind/kind-slice';
+import { orderFeature } from './feature/order/order-slice'
+
+store.subscribe(()=>{
+    // console.log(store.getState());
+})
+
+store.dispatch(kindFeature.initStore());
+
+store.dispatch(kindFeature.actions.storeAdded({
+    kind: {
+        id: 1,
+        name: 'kind 1'
+    },
+    quantity: 50
+}))
+
+store.dispatch(kindFeature.actions.storeRemoved({
+    kind: {
+        id: 1,
+        name: 'kind 1'
+    },
+    quantity: 4
+}))
+
+store.dispatch(orderFeature.actions.orderAdded({
+    id: 1,
+    items: [
+        {kindId: 1, quantity: 5, price: 5000}
+    ],
+    userId: 1
+}))
