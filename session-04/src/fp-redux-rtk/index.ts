1b2d0a4abc87e8b3e42953eea030c94a64339a8e
mostafa <mostafa8026@gmail.com>
2022-11-26 08:18:28 +0330

    update folder structure

---
+++ session-04/src/fp-redux-rtk/index.ts
@@ -0,0 +1,31 @@
+import produce from "immer";
+import { orderFeature } from "./fp-redux-rtk/app/features/order/order-slicer";
+import { fetchUserInfo, userFeature } from "./fp-redux-rtk/app/features/user/user-slice";
+import { store } from "./fp-redux-rtk/app/store";
+import * as _ from 'lodash'
+
+const x = { a: { a: 1 } }
+const y = { a: { b: 1 } }
+const z = { ...x, ...y }
+console.log(z)
+console.log(_.merge(x, y))
+
+const option:{
+    test: any
+} = {test: 0}
+
+const n = produce(option, draft => {
+    draft.test = draft.test || {
+        ali: 50
+    }
+})
+
+console.log('before', option)
+console.log('after', n)
+
+// store.subscribe(()=>{
+//     console.log(store.getState())
+// })
+// store.dispatch(orderFeature.orderActions.orderAdded(1));
+
+// store.dispatch(fetchUserInfo(45))
--- session-04/src/index.ts
+++ session-04/src/index.ts
@@ -1,10 +1,3 @@
-import { orderFeature } from "./app/features/order/order-slicer";
-import { fetchUserInfo, userFeature } from "./app/features/user/user-slice";
-import { store } from "./app/store";
+import { run } from "./redux-observable";
 
-store.subscribe(()=>{
-    console.log(store.getState())
-})
-store.dispatch(orderFeature.orderActions.orderAdded(1));
-
-store.dispatch(fetchUserInfo(45))
+run();
